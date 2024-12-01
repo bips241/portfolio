3f17ff418119853ed3018fb3728b707c3369d042
@bips241 <biplabmal626@gmail.com>
2024-12-02 02:01:02 +0530

    Enhance service and work pages with sliders and animations; update Bulb component

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,14 +1,14 @@
 // data
-const workSlides = {
+export const workSlides = {
   slides: [
     {
       images: [
         {
-          title: 'title',
+          title: 'title1',
           path: '/thumb1.jpg',
         },
         {
-          title: 'title',
+          title: 'title2',
           path: '/thumb2.jpg',
         },
         {
@@ -44,8 +44,51 @@ const workSlides = {
   ],
 };
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import {BsArrowRight} from 'react-icons/bs';
+import Image  from 'next/image';
+
+import { Pagination } from 'swiper';
+
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+  <Swiper
+    spaceBetween={10}
+    pagination={{ clickable: true }}
+    modules={[Pagination]}
+    className="h-[280px] sm:h-[340px]"
+  >
+    {workSlides.slides.map((slide, index) => {
+      return (
+        <SwiperSlide key={index}>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+          {slide.images.map((image, imgIndex) => {
+            return (
+              <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+                <div key={imgIndex} className='flex items-center justify-center relative overflow-hidden'>
+                  <Image src={image.path} alt='' width={500} height={300}/>
+                  <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all divide-neutral-700"></div>
+                  <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                    <div>
+                      <div>title 1</div>
+                      <div>title 2</div>
+                      <div>icon</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </SwiperSlide>
+      ); 
+  })}
+  </Swiper>
+  );
 };
+
 
 export default WorkSlider;
